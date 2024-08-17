7ed14671e8e2fe19a98082927c32e307bb04953d
JulyCat274 <dds@7ya1.ru>
2024-08-17 14:44:20 +0400

    Домашняя работа по уроку "Пространство имен", результат.

--- module_4_2.py
+++ module_4_2.py
@@ -2,6 +2,6 @@ def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
 
-    inner_function()
+    inner_function() # возвращает None
 
 test_function()
